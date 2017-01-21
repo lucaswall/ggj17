9e8f1f37ff40c5ef9bd0fc94e1021488c2067a3a
Lucas Wall <wall.lucas@gmail.com>
2017-01-20 23:49:26 -0300

    Some difficulty progression.

---
+++ UnityProject/Assets/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public float speedInc;
+
+	void Update() {
+		if ( Time.timeScale > 0.0f ) {
+			Time.timeScale += speedInc * Time.unscaledDeltaTime;
+		}
+	}
+
+}
